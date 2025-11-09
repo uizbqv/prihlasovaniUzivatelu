c49ebfd437981aa881af91202ff5abc5fd84309c
KrystofKos <c23kosar@oakostelec.cz>
2025-11-09 23:23:03 +0100

    json
    s jsonem se dá pracovat ale jeho funkce ještě nejsou

--- PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/IOManager.cs
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/IOManager.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace PrihlasovaniUzivatelu
 {
     internal class IOManager
     {
+        static public void JsonConverter() 
+        {
+            UserManager um = new UserManager();
+            
+            User u = new User()
+
+            string Json = JsonSerializer.Serialize(u);
+            Console.WriteLine(Json);
+
+        }
+
+
+    }
+
+    internal class User
+    {
+        public string username { get; }
+        public string password { get; }
+
+        public User(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
     }
 }
