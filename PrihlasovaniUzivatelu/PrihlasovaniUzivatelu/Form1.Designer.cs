248b9bc8221f8dbe211149c6073f4ad5bafb3c95
martin <martinboiii@seznam.cz>
2025-11-09 22:55:48 +0100

    pridal jsem opraveny projekt, snad jsem nic neposr

---
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PrihlasovaniUzivatelu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameBOX1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passwordBOX1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.logInBUTTON1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PrihlasovaniUzivatelu.Properties.Resources._13_02_27_spielbank_wiesbaden_by_RalfR_093;
+            this.pictureBox1.Location = new System.Drawing.Point(236, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOG IN";
+            // 
+            // usernameBOX1
+            // 
+            this.usernameBOX1.Location = new System.Drawing.Point(392, 269);
+            this.usernameBOX1.Name = "usernameBOX1";
+            this.usernameBOX1.Size = new System.Drawing.Size(100, 20);
+            this.usernameBOX1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // passwordBOX1
+            // 
+            this.passwordBOX1.Location = new System.Drawing.Point(392, 311);
+            this.passwordBOX1.Name = "passwordBOX1";
+            this.passwordBOX1.Size = new System.Drawing.Size(100, 20);
+            this.passwordBOX1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Don\'t have an account?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(414, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Register NOW!";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // logInBUTTON1
+            // 
+            this.logInBUTTON1.Location = new System.Drawing.Point(402, 337);
+            this.logInBUTTON1.Name = "logInBUTTON1";
+            this.logInBUTTON1.Size = new System.Drawing.Size(75, 23);
+            this.logInBUTTON1.TabIndex = 8;
+            this.logInBUTTON1.Text = "Log in";
+            this.logInBUTTON1.UseVisualStyleBackColor = true;
+            this.logInBUTTON1.Click += new System.EventHandler(this.logInBUTTON1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(849, 452);
+            this.Controls.Add(this.logInBUTTON1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.passwordBOX1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernameBOX1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernameBOX1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passwordBOX1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button logInBUTTON1;
+    }
+}
+
---
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/UserManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrihlasovaniUzivatelu
+{
+    internal class UserManager
+    {
+        public static void LogIn(string _username, string _password)
+        {
+            string username = _username;
+            string password = _password;
+            PasswordHasher(_password);
+        }
+        public static void Registration()
+        {
+
+
+
+
+        }
+
+        private static DateTime GetRegistrationDate()
+        {
+            DateTime currentDate = DateTime.Now;
+            return currentDate;
+        }
+
+        private static string PasswordHasher(string _password)//tohle cely neni z my hlavy, NIKDY bych na to neprisel takze se me na to u obhajob neprejte prosim 
+        {
+            const int saltSize = 32;
+            const int keySize = 32;
+            const int iterations = 100000;
+
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] salt = new byte[saltSize];
+            rng.GetBytes(salt);
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(_password, salt, iterations, HashAlgorithmName.SHA256);
+
+            byte[] key = new byte[keySize];
+            string hash = Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(key);
+            rng.Dispose();
+            pbkdf2.Dispose();
+            return hash;
+
+
+        }
+    }
+}
