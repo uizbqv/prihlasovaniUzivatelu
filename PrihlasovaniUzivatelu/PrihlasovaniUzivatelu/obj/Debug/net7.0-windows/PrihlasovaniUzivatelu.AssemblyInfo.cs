fc18a7d339da4048450a27822b9f5cbd2cd695fb
KrystofKos <c23kosar@oakostelec.cz>
2025-11-11 11:17:26 +0100

    nic

--- PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/obj/Debug/net7.0-windows/PrihlasovaniUzivatelu.AssemblyInfo.cs
+++ PrihlasovaniUzivatelu/PrihlasovaniUzivatelu/obj/Debug/net7.0-windows/PrihlasovaniUzivatelu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrihlasovaniUzivatelu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0644a15fc4d23011e5e8d72ef6e22c0ffde52da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d5aec0e65687b2b7503c7e431d53a4706809d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrihlasovaniUzivatelu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrihlasovaniUzivatelu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
